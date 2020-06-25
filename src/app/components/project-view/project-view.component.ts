9e3bccc59ca14e19a94aaa9ec0144db34c6e1bcf
Pablo Kvitca <pablokvitca@gmail.com>
2020-06-25 03:15:37 -0300

    Added contentful service. broken 2nd display

--- src/app/components/project-view/project-view.component.ts
+++ src/app/components/project-view/project-view.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit, Input } from '@angular/core';
-import _ from "lodash";
 import { Project } from './Project';
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import {faExternalLinkAlt, faLevelUpAlt} from '@fortawesome/free-solid-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-project-view',
   templateUrl: './project-view.component.html',
   styleUrls: ['./project-view.component.scss']
 })
-export class ProjectViewComponent implements OnInit {
+export class ProjectViewComponent {
 
   externalLinkIcon = {
     source: 'fontawesome',
     icon: faExternalLinkAlt,
-    alt: "Open",
-    customClasses: "font-light"
-  }
+    alt: 'Open',
+    customClasses: 'font-light'
+  };
+
+  iconDeploymentRef = {
+    source: 'fontawesome',
+    icon: faLevelUpAlt,
+    alt: 'Open',
+    customClasses: 'font-light'
+  };
+
+  iconSourceRef = {
+    source: 'fontawesome',
+    icon: faGithub,
+    alt: 'Open',
+    customClasses: 'font-light'
+  };
 
   @Input()
   public project: Project;
 
   @Input()
-  public reverse: boolean = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public reverse = false;
 
 }
---
+++ src/app/services/contentful.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {createClient, Entry, EntryCollection,} from 'contentful';
+import {Project} from '../components/project-view/Project';
+
+const CONFIG = { // TODO: actual identifiers!!! + .env
+  space: '6pjgrdux22jl',
+  accessToken: 'sLlwHtvfJp4wEb6AdG_OAxw24QWS3OI4lBVa2kPXLkg',
+  contentEnvironment: 'master',
+  contentTypeIds: {
+    project: 'project',
+    icon: 'icon',
+    technologies: 'technologyReference'
+  }
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentfulService {
+
+  private cdaClient = createClient({
+    space: CONFIG.space,
+    accessToken: CONFIG.accessToken,
+    environment: CONFIG.contentEnvironment
+  });
+
+  constructor() { }
+
+  getProjects(query?: object): Promise<Entry<any>[]> {
+    return this.cdaClient.getEntries(Object.assign({
+      content_type: CONFIG.contentTypeIds.project,
+      include: 5
+    }, query))
+      .catch((err) => {
+        // TODO: handle error
+      })
+      .then((res: EntryCollection<any>) => {
+        return res.items;
+      });
+  }
+}
