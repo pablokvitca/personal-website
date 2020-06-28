e37c9b2d815561445900c027505f76b2085a88f5
Pablo Kvitca <pablokvitca@gmail.com>
2020-06-28 01:25:34 -0300

    New color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,51 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': { // Space Cadet
+          'darker': '#0d1122',
+          'dark': '#1d2132',
+          'medium': '#2d3142',
+          '': '#2d3142',
+          'light': '#4d5162',
+          'lighter': '#5d6172'
+        },
+        'primary': { // Steel Teal
+          'darker': '#10454b',
+          'dark': '#30656b',
+          'medium': '#50858b',
+          '': '#50858b',
+          'light': '#70a5ab',
+          'lighter': '#90c5cb'
+        },
+        'secondary': { // Blue Munsell
+          'darker': '#106060',
+          'dark': '#347787',
+          'medium': '#5497a7',
+          '': '#5497a7',
+          'light': '#74b7c7',
+          'lighter': '#94d7e7'
+        },
+        'accent': { // Auburn
+          'darker': '#500104',
+          'dark': '#820c09',
+          'medium': '#a22c29',
+          '': '#a22c29',
+          'light': '#b24059',
+          'lighter': '#c24c59'
+        },
+        't-white': { // Alabaster
+          'darker': '#b1b0aa',
+          'dark': '#d1d0ca',
+          'medium': '#f1f0ea',
+          '': '#f1f0ea',
+          'light': '#fafafa',
+          'lighter': '#ffffff'
+        },
+      }
+    },
   },
   variants: {},
   plugins: [],
-}
+};
